f08997b11643d0af50004602480bcd50dfb916d2
Gayathri <gayathri.nelavigi@wipro.com>
2018-11-19 13:56:49 +0530

    added .travis folder with build.sh file

---
+++ .travis/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+ 
+chmod u+x gradlew
+ 
+if [ "$TRAVIS_PULL_REQUEST" != "false" ];
+then
+    echo "Skip integration tests in pull request builds"
+    ./gradlew clean build -x integrationTest
+else
+    ./gradlew clean build
+fi
